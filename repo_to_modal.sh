1fc232aef801b00b8ce76807740881b71f8ffb98
skadio <serdark@cs.brown.edu>
2025-12-21 17:33:20 -0500

    repo_to_modal.sh script to copy code to modal and use from notebooks

--- repo_to_modal.sh
+++ repo_to_modal.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-# Create the volume: `modal volume create forge_repo`
+# ONE TIME: create the volume: `modal volume create forge_repo`
+
+# Clean-up previous code from the volume
+modal volume rm forge_repo data -r
+modal volume rm forge_repo forge -r
+modal volume rm forge_repo scripts -r
+
+# Add the latest code to the volume
 modal volume put forge_repo ./data/configs/ ./data/configs/ --force
 modal volume put forge_repo ./forge ./forge --force
 modal volume put forge_repo ./scripts ./scripts --force
